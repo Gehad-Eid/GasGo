256f3dd88abf2fb41056b96e101f13f9c612e651
Abraralhazza <pealhazza9@gmail.com>
2023-10-19 10:02:40 +0300

    chenge

--- GASGO./stationDetailsSheet.swift
+++ GASGO./stationDetailsSheet.swift
@@ -29,14 +29,14 @@ struct stationDetailsSheet: View {
                 .font(.title)
             
             VStack{
-
+                
                 HStack{
                     Text("\(selectedItem.locations[selectedItemIndex].rate, specifier: "%.2f")\t  ")
                         .offset(x:10 ,y:1)
                     
                     Image(systemName: "star.fill")
                         .foregroundColor(Color.yellow)
-                    Image(systemName: "star.fill")       
+                    Image(systemName: "star.fill")
                         .foregroundColor(Color.yellow)
                     Image(systemName: "star.fill")
                         .foregroundColor(Color.yellow)
@@ -54,21 +54,21 @@ struct stationDetailsSheet: View {
                 
                 VStack{
                     Text(selectedItem.locations[selectedItemIndex].isOpened ? "Open" : "Closed")
-                   .offset(x:13 ,y:-33)
-                   .foregroundColor(Color(hue: 0.409, saturation: 0.991, brightness: 0.443))
-                   .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .offset(x:13 ,y:-33)
+                        .foregroundColor(Color(hue: 0.409, saturation: 0.991, brightness: 0.443))
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
                     Text("\t\t Closeing at \(selectedItem.locations[selectedItemIndex].closeAt)")
-                   .multilineTextAlignment(.leading)
-                   .offset(x:-82 ,y:-58)
-                   .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .offset(x:-82 ,y:-58)
+                        .foregroundColor(Color.black)
                 }
                 .padding(.top,25)
-           
+                
                 
                 
                 HStack(alignment: .top){
                     
-                                        
+                    
                     Button("Start"){
                         //opens the location in Maps App
                         var selectedLocationMapItem = MKMapItem(placemark: MKPlacemark(coordinate: selectedItem.locations[selectedItemIndex].coordinate))
@@ -88,68 +88,49 @@ struct stationDetailsSheet: View {
                     .buttonStyle(.borderedProminent)
                     .tint(selectedItem.locations[selectedItemIndex].isFav == true ? .yellow : Color(red: 0.00392156862745098, green: 0.2823529411764706, blue: 0.44313725490196076))
                     .offset(x:-91 ,y:-58)
-//                    .offset(x:-39 ,y:-58)
+                    //                    .offset(x:-39 ,y:-58)
                 }
                 
-//                LazyVGrid(columns: [
-//                    GridItem(.flexible(), spacing: 10),
-//                    GridItem(.flexible(), spacing: 10),
-//                    GridItem(.flexible(), spacing: 10),
-//                    GridItem(.flexible(), spacing: 10)
-//                ], spacing: 30) {
-//                    ForEach(selectedItem.locations[selectedItemIndex].service, id: \.self) { iconName in
-//                                    Image(systemName: iconName)
-//                                        .font(.system(size: 30))
-//                                        .frame(width: 60, height: 60)
-//                                        .background(Color(hex: 0x9ABCAB))
-//                                        .foregroundColor(.white)
-//                                        .cornerRadius(10)
-//                                }
-//                            }
-//                .padding(.top,-40)
-//                .padding(.leading,8)
-//                .padding(.trailing, 62)
+                //                LazyVGrid(columns: [
+                //                    GridItem(.flexible(), spacing: 10),
+                //                    GridItem(.flexible(), spacing: 10),
+                //                    GridItem(.flexible(), spacing: 10),
+                //                    GridItem(.flexible(), spacing: 10)
+                //                ], spacing: 30) {
+                //                    ForEach(selectedItem.locations[selectedItemIndex].service, id: \.self) { iconName in
+                //                                    Image(systemName: iconName)
+                //                                        .font(.system(size: 30))
+                //                                        .frame(width: 60, height: 60)
+                //                                        .background(Color(hex: 0x9ABCAB))
+                //                                        .foregroundColor(.white)
+                //                                        .cornerRadius(10)
+                //                                }
+                //                            }
+                //                .padding(.top,-40)
+                //                .padding(.leading,8)
+                //                .padding(.trailing, 62)
                 
                 LazyVGrid(columns: [GridItem(.flexible(), spacing: 16), GridItem(.flexible(), spacing: 16), GridItem(.flexible(), spacing: 16)], spacing: 16) {
-                                ForEach(selectedItem.locations[selectedItemIndex].service, id: \.self) { iconName in
-                                    VStack {
-                                        Image(systemName: iconName)
-                                            .font(.system(size: 30))
-                                            .frame(width: 60, height: 60)
-                                            .background(Color(hex: 0x9ABCAB))
-                                            .foregroundColor(.white)
-                                            .cornerRadius(10)
-                                        Text(iconTextMapping[iconName] ?? "Unknown")
-                                            .font(.caption)
-                                            .padding(.top, 4)
-                                    }
-                                }
-                            }
+                    ForEach(selectedItem.locations[selectedItemIndex].service, id: \.self) { iconName in
+                        VStack {
+                            Image(systemName: iconName)
+                                .font(.system(size: 30))
+                                .frame(width: 60, height: 60)
+                                .background(Color(hex: 0x9ABCAB))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                            Text(iconTextMapping[iconName] ?? "Unknown")
+                                .font(.caption)
+                                .padding(.top, 4)
+                        }
+                    }
+                }
                 .padding(.top,-40)
-                                .padding(.leading,8)
-                                .padding(.trailing, 62)
-                
-     
-//               VStack{
-//                   HStack{
-//                       Image("sa")
-//                      .frame(width:190,height: 192)
-//                     .cornerRadius(16)
-//                     .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-//                       Image("ar")
-//                  .renderingMode(.original).frame(width:190,height: 192)
-//                     .cornerRadius(16)
-//                     .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-//                      
-//                               
-//                           }
-//                       }
-                
-                   }
+                .padding(.leading,8)
+                .padding(.trailing, 62)
+            }
             .padding(.leading,20)
-            
-            
-               }
+        }
         .padding(.top, 20)
         .padding(.leading, 10)
         }
