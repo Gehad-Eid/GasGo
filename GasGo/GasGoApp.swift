02cdc187cf6cd8b74dd24283eeae395d8554aaa3
gehad1995 <gehad.gehad1995@gmail.com>
2024-02-13 20:21:50 +0300

    Initial Commit

---
+++ GasGo/GasGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasGoApp.swift
+//  GasGo
+//
+//  Created by Gehad Eid on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GasGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
